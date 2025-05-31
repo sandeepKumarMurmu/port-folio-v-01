537e9b32a882e5a48cb00e38f4be8146e7445359
ALLEN-SandeepKumarMurmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2025-05-31 22:26:32 +0530

    contact us page done

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -65,87 +65,82 @@ function GetInTouch() {
     }
 
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen z-50 backdrop-blur flex justify-center items-center">
-            <section className="relative w-full max-w-5xl mx-auto p-6 shadow-lg rounded-xl bg-base-300 flex flex-row">
-
-                {/* Close Button */}
-                <button
-                    className="absolute top-4 right-4 btn btn-sm btn-circle bg-transparent border-0 shadow-none transition-transform duration-300 hover:rotate-90"
-                    aria-label="Close"
-                    onClick={toggle}
-                >
-                    <CircleX className="w-5 h-5" />
-                </button>
-
-
-                <div className="flex-1 flex items-center justify-center bg-base-200 rounded-xl p-6">
-                    <div className="text-center space-y-4">
-                        <h2 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary drop-shadow-md">
-                            Get in Touch
-                        </h2>
-                        <p className="text-base text-base-content leading-relaxed max-w-md mx-auto">
-                            Have a question, project idea, or just want to say hello? Fill out the form below and I'll get back to you soon!
-                        </p>
+        <>
+            <div className='fixed top-0 left-0 w-screen h-screen z-50 backdrop-blur flex justify-center items-center px-4'>
+                <section className='w-full max-w-[1440px] mx-auto p-4 shadow-lg rounded-xl bg-base-300 flex flex-col'>
+                    <div className='flex items-center '>
+                        <button
+                            className="top-4 right-4 btn btn-sm btn-circle bg-transparent border-0 shadow-none transition-transform duration-300 hover:rotate-90"
+                            aria-label="Close"
+                            onClick={toggle}
+                        >
+                            <CircleX className="w-5 h-5" />
+                        </button>
                     </div>
-                </div>
+                    <div className='w-full grid lg:grid-cols-2 items-center'>
+                        <div className='text-center'>
+                            <h2 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary drop-shadow-md">
+                                Get in Touch
+                            </h2>
+                            <p className="text-base text-base-content leading-relaxed max-w-md mx-auto">
+                                Have a question, project idea, or just want to say hello? Fill out the form below and I'll get back to you soon!
+                            </p>
+                        </div>
+                        <div>
+                            <form className="space-y-4 flex-1" onSubmit={handeSubmit}>
+                                <div>
+                                    <label className="block text-sm font-semibold text-primary mb-1">
+                                        Name
+                                    </label>
+                                    <input
+                                        name="name"
+                                        onChange={handelChange}
+                                        value={formData.name}
+                                        type="text"
+                                        placeholder="Your full name"
+                                        className="input input-bordered input-primary w-full"
+                                    />
+                                </div>
 
-                {/* Vertical separator */}
-                <div className="divider divider-horizontal" />
+                                <div>
+                                    <label className="block text-sm font-semibold text-primary mb-1">
+                                        Email
+                                    </label>
+                                    <input
+                                        name="email"
+                                        onChange={handelChange}
+                                        value={formData.email}
+                                        type="email"
+                                        placeholder="you@example.com"
+                                        className="input input-bordered input-primary w-full"
+                                    />
+                                </div>
 
-                <form className="space-y-4 flex-1" onSubmit={handeSubmit}>
-                    <div>
-                        <label className="block text-sm font-semibold text-primary mb-1">
-                            Name
-                        </label>
-                        <input
-                            name="name"
-                            onChange={handelChange}
-                            value={formData.name}
-                            type="text"
-                            placeholder="Your full name"
-                            className="input input-bordered input-primary w-full"
-                        />
+                                <div>
+                                    <label className="block text-sm font-semibold text-primary mb-1">
+                                        Message
+                                    </label>
+                                    <textarea
+                                        name="message"
+                                        onChange={handelChange}
+                                        value={formData.message}
+                                        placeholder="Tell me about your project or message..."
+                                        className="textarea textarea-primary w-full resize-none"
+                                    ></textarea>
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary w-full"
+                                >
+                                    Send Message
+                                </button>
+                            </form>
+                        </div>
                     </div>
-
-                    <div>
-                        <label className="block text-sm font-semibold text-primary mb-1">
-                            Email
-                        </label>
-                        <input
-                            name="email"
-                            onChange={handelChange}
-                            value={formData.email}
-                            type="email"
-                            placeholder="you@example.com"
-                            className="input input-bordered input-primary w-full"
-                        />
-                    </div>
-
-                    <div>
-                        <label className="block text-sm font-semibold text-primary mb-1">
-                            Message
-                        </label>
-                        <textarea
-                            name="message"
-                            onChange={handelChange}
-                            value={formData.message}
-                            placeholder="Tell me about your project or message..."
-                            className="textarea textarea-primary w-full resize-none"
-                        ></textarea>
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="btn btn-primary w-full"
-                    >
-                        Send Message
-                    </button>
-                </form>
-            </section>
-
-        </div>
-
-
+                </section>
+            </div>
+        </>
     )
 }
 
